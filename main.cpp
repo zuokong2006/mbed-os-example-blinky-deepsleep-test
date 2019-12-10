341f38efd4c43ef4c51be9dd747dfdeec9bdd89e
David Zuo <David.Zuo@cypress.com>
2019-12-10 15:30:54 +0800

    Add the example source code.

---
+++ main.cpp
@@ -0,0 +1,36 @@
+/* mbed Microcontroller Library
+ * Copyright (c) 2019 ARM Limited
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include "mbed.h"
+#include "platform/mbed_thread.h"
+
+
+// Blinking rate in milliseconds
+#define BLINKING_RATE_MS                5000
+
+
+int main()
+{
+    printf("\r\n\r\nmbed-os-example-blinky start...\r\n");
+    // Initialise the digital pin LED1 as an output
+    DigitalOut led(LED1);
+
+    while (true) {
+        printf("\r\nturn off LED\r\n");
+        led = 1;
+        printf("thread_sleep_for\r\n");
+        thread_sleep_for(BLINKING_RATE_MS);
+
+        printf("\r\nturn on LED\r\n");
+        led = 0;
+        printf("sleep_manager_lock_deep_sleep\r\n");
+        sleep_manager_lock_deep_sleep();
+        printf("thread_sleep_for\r\n");
+        thread_sleep_for(BLINKING_RATE_MS);
+        printf("sleep_manager_unlock_deep_sleep\r\n");
+        sleep_manager_unlock_deep_sleep();
+    }
+}
+
