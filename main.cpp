5b52fe0d09b6361c3a56597f69cf521a7fc30a99
David Zuo <David.Zuo@cypress.com>
2019-12-24 15:48:34 +0800

    Add the active code.

--- main.cpp
+++ main.cpp
@@ -16,21 +16,21 @@ int main()
     printf("\r\n\r\nmbed-os-example-blinky start...\r\n");
     // Initialise the digital pin LED1 as an output
     DigitalOut led(LED1);
+    led = 1;
 
     while (true) {
-        printf("\r\nturn off LED\r\n");
-        led = 1;
-        printf("thread_sleep_for\r\n");
+        printf("Deep sleep: thread_sleep_for\r\n");
         thread_sleep_for(BLINKING_RATE_MS);
 
-        printf("\r\nturn on LED\r\n");
-        led = 0;
         printf("sleep_manager_lock_deep_sleep\r\n");
         sleep_manager_lock_deep_sleep();
-        printf("thread_sleep_for\r\n");
+        printf("Sleep: thread_sleep_for\r\n");
         thread_sleep_for(BLINKING_RATE_MS);
         printf("sleep_manager_unlock_deep_sleep\r\n");
         sleep_manager_unlock_deep_sleep();
+        
+        printf("Active: Cy_SysLib_Delay\r\n");
+        Cy_SysLib_Delay(BLINKING_RATE_MS);
     }
 }
 
